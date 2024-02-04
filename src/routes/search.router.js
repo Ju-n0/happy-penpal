809076a1b970b82e6bc54d4eb912a297f1bb319f
Ju-n0 <juliettenogueira.pro@gmail.com>
2024-02-04 22:43:54 +0100

    search page integration added

--- src/routes/search.router.js
+++ src/routes/search.router.js
@@ -4,7 +4,7 @@ const searchController = require("../controllers/searchController.js");
 
 const router = new Router();
 
-// router.get("/search", searchController.index);
+router.get("/search", searchController.index);
 // router.post("/search", searchController.getProfiles);
 
 module.exports = router;
